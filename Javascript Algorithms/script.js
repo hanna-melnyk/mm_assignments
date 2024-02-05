f5e0e0848d48bf9ffb6e77a3774e6e3d479bc88f
hanna-melnyk <hanna.melnk@gmail.com>
2024-02-05 17:03:23 +0100

    Added all of the Monday assignments

--- Javascript Algorithms/script.js
+++ Javascript Algorithms/script.js
@@ -97,3 +97,82 @@ const numToStr = (array) => {
 }
 
 console.log(numToStr(newArrayXc));
+
+
+// JS Intermediate
+// Largest number in Subarray
+
+let arrayOfArrays = [[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]];
+function largestInSubarray(array) {
+  var newArray = [];
+  for (let i = 0; i < array.length; i++) {
+    var largestNumInI = array[i][0];
+    for (let j = 0; j < array[i].length; j++) {
+      if (array[i][j] > largestNumInI) {
+        largestNumInI = array[i][j];
+      }
+    }
+    newArray[i] = largestNumInI;
+  }
+  return newArray;
+}
+
+// console.log(largestInSubarray(arrayOfArrays));
+
+
+//Professional Algorithms
+// Game of 3s
+// Simplified Game of 3s
+function gameOfThree(number) {
+  while (number !== 1) { // Continue until the number is 1
+    let adjustment = 0; // Adjustment to make the number divisible by 3
+
+
+    if (number % 3 === 1) {
+      adjustment = -1; // Subtract 1 if remainder is 1
+    } else if (number % 3 === 2) {
+      adjustment = 1; // Add 1 if remainder is 2
+    }
+
+    console.log(`${number}, ${adjustment}`); // Log current step
+    number = (number + adjustment) / 3; // Adjust the number
+  }
+  console.log(1); // Print the final number, 1
+}
+
+gameOfThree(100);
+
+
+// rFactorial vs iFactorial
+
+
+//rFactorial
+// Prompt user to enter a number to calculate the factorial
+var num = prompt("rFactorial: What number do you want to find the factorial of?");
+
+
+var rFactorial = function(n) {
+  if (n == 0) {
+    return 1;
+  } else {
+    return n * rFactorial(n - 1);
+  }
+}
+
+console.log(rFactorial(num));
+
+// iFactorial
+
+// Prompt user to enter a number to calculate the factorial
+var num = prompt("iFactorial: What number do you want to find the factorial of?");
+
+
+function iFactorial(n) {
+  let result = 1;
+  for (let i = 2; i <= n; i++) {
+    result *= i;
+  }
+  return result;
+}
+
+console.log(iFactorial(num));
